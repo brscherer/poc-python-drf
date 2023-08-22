0b8bc6509dadd956ca8b80adf00957fedf2a8834
Bruno Scherer <brscherer@lendingclub.com>
2023-08-21 23:17:48 -0300

    finish todo CRUD

--- caching_poc/caching_poc/settings.py
+++ caching_poc/caching_poc/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'caching_poc',
-    'api'
+    'caching_poc'
 ]
 
 MIDDLEWARE = [
--- caching_poc/caching_poc/urls.py
+++ caching_poc/caching_poc/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
-from caching_poc import views
+
+from .views import TodoListApiView, TodoDetailApiView
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('todos/', TodoListApiView.as_view()),
+    path('todos/<int:todo_id>/', TodoDetailApiView.as_view()),
+
 ]
